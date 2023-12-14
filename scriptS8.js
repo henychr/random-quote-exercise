875df311d77c86d4d691e9ae0b66d8cea51603d2
Radek Henych <henychr@gmail.com>
2023-12-14 22:17:14 +0100

    Step4

---
+++ scriptS8.js
@@ -0,0 +1,20 @@
+
+const loadQuote = async () =>{
+      const response = await fetch('#')
+}
+
+const p = document.getElementById("rq-text");
+
+const btn = document.getElementById("button-load");
+
+console.log(p);
+console.log(btn);
+
+btn.addEventListener("click", () => {
+    loadQuote
+})
+
+
+// const newQuote = btn.addEventListener("click", () => {
+//     p.innerHTML = response;
+//  })
