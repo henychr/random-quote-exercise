c30838523cf203aeef6dbbcc803590e7ef26d5ff
Radek Henych <henychr@gmail.com>
2023-12-26 18:56:48 +0100

    finito

--- scriptS8.js
+++ scriptS8.js
@@ -1,19 +1,15 @@
-const loadQuote = async () => {
-    const response = await fetch('https://api.quotable.io/random');
-    const data = await response.json();
-    const newText = data.content;
-    console.log(newText);
-    return newText
-}
 
 const p = document.getElementById("rq-text");
 const btn = document.getElementById("button-load");
 
-console.log(p);
-console.log(btn);
-
-btn.addEventListener("click", () => {
-    loadQuote();
+const loadQuote = async () => {
+    const response = await fetch('https://api.quotable.io/random');
+    const data = await response.json();
+    const newQuote = data.content;
+    console.log(newQuote);
+    return newQuote
+}
+btn.addEventListener("click", async() => {
+    p.innerHTML = await loadQuote()
 })
 
-p.innerHTML = newText;
